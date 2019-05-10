ff83cdf5a5602a5fed99140c8771f3c27ca048aa
andrewgray0702 <andrew.gray@midlandu.edu>
2019-05-10 16:32:31 -0500

    -

--- weeklyFishFacts/server.js
+++ weeklyFishFacts/server.js
@@ -4,14 +4,8 @@ const app = express();
 const PORT = 3000;
 const userRoutes = require('./routes/user.routes');
 const factRoutes = require('./routes/facts.routes');
-const cors = require('cors');
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-}
+
 
 
 app.use(allowCrossDomain);
